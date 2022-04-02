b123bd2d8f2473e55cb8d6ebe81be661b0c60486
Sam11-ss <84765714+Sam11-ss@users.noreply.github.com>
2022-04-02 12:13:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var school_email = document.getElementById("school_email_input").value;
+var bully_name = document.getElementById("bully_name_input").value;
+var bully_class = document.getElementById("bully_class_input").value
